ba7c97d012342782398b9eccb5f585858b71b8e9
Tejas Mehta <tmthecoder@gmail.com>
2024-03-26 15:13:41 -0400

    Base CLI app

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
+
+yargs(hideBin(process.argv))
+    .command('greet [name]', 'Greet a user', (yargs) => {
+        return yargs.positional('name', {
+            describe: 'Name to greet',
+            type: 'string',
+        });
+    }, (argv) => {
+        if (argv.name) {
+            console.log(`Hello, ${argv.name}!`);
+        } else {
+            console.log('Hello, world!');
+        }
+    })
+    .parse();
+
