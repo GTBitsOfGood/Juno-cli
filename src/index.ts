3e513623a878b1979f59c1d0dafc16a779332626
Syaam Khandaker <syaamkhandaker@gmail.com>
2024-03-28 17:14:03 -0400

    init w/ adding tsx dependency

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,25 @@
-import yargs from 'yargs';
-import { hideBin } from 'yargs/helpers';
+import { hideBin } from "yargs/helpers";
+import yargs from "yargs";
 
 yargs(hideBin(process.argv))
-    .command('greet [name]', 'Greet a user', (yargs) => {
-        return yargs.positional('name', {
-            describe: 'Name to greet',
-            type: 'string',
-        });
-    }, (argv) => {
-        if (argv.name) {
-            console.log(`Hello, ${argv.name}!`);
-        } else {
-            console.log('Hello, world!');
-        }
-    })
-    .demandCommand(1, 'Please enter a command')
-    .strict()
-    .showHelpOnFail(true)
-    .parse();
-
+  .command(
+    "greet [name]",
+    "Greet a user",
+    (yargs) => {
+      return yargs.positional("name", {
+        describe: "Name to greet",
+        type: "string",
+      });
+    },
+    (argv) => {
+      if (argv.name) {
+        console.log(`Hello, ${argv.name}!`);
+      } else {
+        console.log("Hello, world!");
+      }
+    }
+  )
+  .demandCommand(1, "Please enter a command")
+  .strict()
+  .showHelpOnFail(true)
+  .parse();
